5416a4bf31256e41b8af1e918e576cf1ea89841b
Alfredo Stanquini Neto <stanquini@gmail.com>
2021-08-03 22:15:42 -0300

    adiciona numero de itens inclusos no cart no botao do header + listagem de de produtos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
-
-import { ProductList } from './styles';
-import { api } from '../../services/api';
-import { formatPrice } from '../../util/format';
 import { useCart } from '../../hooks/useCart';
+import { api } from '../../services/api';
+import { ProductList } from './styles';
+
+
+// import { formatPrice } from '../../util/format';
+
 
 interface Product {
   id: number;
@@ -22,16 +24,20 @@ interface CartItemsAmount {
 }
 
 const Home = (): JSX.Element => {
-  // const [products, setProducts] = useState<ProductFormatted[]>([]);
-  // const { addProduct, cart } = useCart();
+  const [products, setProducts] = useState<ProductFormatted[]>([]);
+  const { addProduct, cart } = useCart();
+  // console.log(cart)
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    const sumAmountCart = { ...sumAmount }
+    sumAmountCart[product.id] = product.amount
+    return sumAmountCart;
+  }, {} as CartItemsAmount)
 
   useEffect(() => {
     async function loadProducts() {
-      // TODO
+      const products = await api.get('products')
+      setProducts(products.data)
     }
 
     loadProducts();
@@ -43,23 +49,31 @@ const Home = (): JSX.Element => {
 
   return (
     <ProductList>
-      <li>
-        <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
-        <strong>Tênis de Caminhada Leve Confortável</strong>
-        <span>R$ 179,90</span>
-        <button
-          type="button"
-          data-testid="add-product-button"
-        // onClick={() => handleAddProduct(product.id)}
-        >
-          <div data-testid="cart-product-quantity">
-            <MdAddShoppingCart size={16} color="#FFF" />
-            {/* {cartItemsAmount[product.id] || 0} */} 2
-          </div>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{Intl.NumberFormat('pt-BR',
+            {
+              style: 'currency',
+              currency: 'BRL'
+            }).format(product.price)}
+          </span>
+          <button
+            type="button"
+            data-testid="add-product-button"
+          // onClick={() => handleAddProduct(product.id)}
+          >
+            <div data-testid="cart-product-quantity">
+              <MdAddShoppingCart size={16} color="#FFF" />
+              {cartItemsAmount[product.id] || 0}
+            </div>
 
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
+            <span>ADICIONAR AO CARRINHO</span>
+          </button>
+        </li>
+
+      ))}
     </ProductList>
   );
 };
